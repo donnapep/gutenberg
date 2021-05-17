4b9438f06e63dc58210e07dd3d42834829cd2433
Jorge <jorge.costa@developer.pt>
2021-05-17 15:33:10 +0200

    Update: Use elements mechanism for link color instead of a CSS variable
    Co-authored-by: André <nosolosw@users.noreply.github.com> (+3 squashed commits)
    Squashed commits:
    [994e5e3beb] Add link color as an element mechanism.
    [e4a72bcb00] Remove existing link color
    [7fcd57c9a6] Fix nav placeholder colors and height. (#31875)

--- packages/block-editor/src/hooks/style.js
+++ packages/block-editor/src/hooks/style.js
@@ -1,7 +1,19 @@
 /**
  * External dependencies
  */
-import { capitalize, get, has, omit, omitBy, startsWith } from 'lodash';
+import {
+	capitalize,
+	kebabCase,
+	get,
+	has,
+	map,
+	omit,
+	omitBy,
+	startsWith,
+	isEmpty,
+	first,
+} from 'lodash';
+import classnames from 'classnames';
 
 /**
  * WordPress dependencies
@@ -11,8 +23,9 @@ import {
 	getBlockSupport,
 	hasBlockSupport,
 	__EXPERIMENTAL_STYLE_PROPERTY as STYLE_PROPERTY,
+	__EXPERIMENTAL_ELEMENTS as ELEMENTS,
 } from '@wordpress/blocks';
-import { createHigherOrderComponent } from '@wordpress/compose';
+import { createHigherOrderComponent, useInstanceId } from '@wordpress/compose';
 
 /**
  * Internal dependencies
@@ -30,6 +43,8 @@ const styleSupportKeys = [
 	COLOR_SUPPORT_KEY,
 	SPACING_SUPPORT_KEY,
 ];
+
+
 
 const hasStyleSupport = ( blockType ) =>
 	styleSupportKeys.some( ( key ) => hasBlockSupport( blockType, key ) );
@@ -59,7 +74,8 @@ export function getInlineStyles( styles = {} ) {
 	Object.keys( STYLE_PROPERTY ).forEach( ( propKey ) => {
 		const path = STYLE_PROPERTY[ propKey ].value;
 		const subPaths = STYLE_PROPERTY[ propKey ].properties;
-		if ( has( styles, path ) ) {
+		// Ignore styles on elements because they are handled on the server.
+		if ( has( styles, path ) && 'elements' !== first( path ) ) {
 			if ( !! subPaths ) {
 				subPaths.forEach( ( suffix ) => {
 					output[
@@ -73,6 +89,26 @@ export function getInlineStyles( styles = {} ) {
 	} );
 
 	return output;
+}
+
+function compileElementsStyles( selector, elements = {} ) {
+	return map( elements, ( styles, element ) => {
+		const elementStyles = getInlineStyles( styles );
+		if ( ! isEmpty( elementStyles ) ) {
+			return [
+				`.${ selector } ${ ELEMENTS[ element ] }{`,
+				...map(
+					elementStyles,
+					( value, property ) =>
+						`\t${ kebabCase( property ) }: ${ value }${
+							element === 'link' ? '!important' : ''
+						};`
+				),
+				'}',
+			].join( '\n' );
+		}
+		return '';
+	} ).join( '\n' );
 }
 
 /**
@@ -202,6 +238,45 @@ export const withBlockControls = createHigherOrderComponent(
 	'withToolbarControls'
 );
 
+/**
+ * Override the default block element to include duotone styles.
+ *
+ * @param  {Function} BlockListBlock Original component
+ * @return {Function}                Wrapped component
+ */
+const withElementsStyles = createHigherOrderComponent(
+	( BlockListBlock ) => ( props ) => {
+		const elements = props.attributes.style?.elements;
+		if ( ! elements ) {
+			return <BlockListBlock { ...props } />;
+		}
+		const blockElementsContainerIdentifier = `wp-elements-${ useInstanceId(
+			BlockListBlock
+		) }`;
+		const styles = compileElementsStyles(
+			blockElementsContainerIdentifier,
+			props.attributes.style?.elements
+		);
+
+		return (
+			<>
+				<style
+					dangerouslySetInnerHTML={ {
+						__html: styles,
+					} }
+				/>
+				<BlockListBlock
+					{ ...props }
+					className={ classnames(
+						props.classname,
+						blockElementsContainerIdentifier
+					) }
+				/>
+			</>
+		);
+	}
+);
+
 addFilter(
 	'blocks.registerBlockType',
 	'core/style/addAttribute',
@@ -224,4 +299,10 @@ addFilter(
 	'editor.BlockEdit',
 	'core/style/with-block-controls',
 	withBlockControls
+);
+
+addFilter(
+	'editor.BlockListBlock',
+	'core/editor/with-elements-styles',
+	withElementsStyles
 );
