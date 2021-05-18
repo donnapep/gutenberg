dc6fc8c315ee1edfb45fbfb53364b3ceac37dc0e
André <andres.maneiro@automattic.com>
2021-05-18 11:33:45 +0200

    Color palette and gradients are merged

--- phpunit/class-wp-theme-json-test.php
+++ phpunit/class-wp-theme-json-test.php
@@ -407,6 +407,14 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 					'customGradient' => true,
 					'palette'        => array(
 						array(
+							'slug'  => 'red',
+							'color' => 'red',
+						),
+						array(
+							'slug'  => 'green',
+							'color' => 'green',
+						),
+						array(
 							'slug'  => 'blue',
 							'color' => 'blue',
 						),
